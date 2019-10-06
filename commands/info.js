7ff3bd65cbedba605718b72d17096e2e3ef6d473
V0xt <alex.5363@ya.ru>
2019-10-06 19:13:26 +0300

    Removed unnecessary files

--- commands/info.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'info',
-	usage: '!info',
-	description: 'Info command.',
-	execute(message) {
-		message.channel.send('Type `!help` to get full list of available commands\n' + 
-			'Follow the link to get more info about the bot https://github.com/V0xt/priyanka_bot');
-	},
-};
--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'ping',
-	usage: '!ping',
-	description: 'Returns \'Pong!\'',
-	execute(message) {
-		message.channel.send('Pong!');
-	},
-};
