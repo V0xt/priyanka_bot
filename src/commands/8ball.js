4aa6e10083088136d7b422f88b64b9e4f566f300
V0xt <alex.5363@ya.ru>
2019-10-09 23:04:04 +0300

    8ball refactor: answers, description

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -1,15 +1,21 @@
 module.exports = {
 	name: '8ball',
 	usage: '!8ball',
-	description: 'Returns random answer.',
+	aliases: '',
+	description: `8-Ball reaches into the future, to find the answers to your questions. It knows what will be, and is willing to share this with you.\nJust think of a question that can be answered "Yes" or "No", concentrate very, very hard, and type \'!8ball\'. Then let 8-Ball show you the way!`,
 	execute(message) {
 		let answers = [
-			'yes',
-			'no',
-			'definetly not',
-			'maybe'
+			'YES',
+			'NO',
+			'DEFINETLY NOT',
+			'MAYBE',
+			'WITHOUT A DOUBT',
+			'OUTLOOK NOT SO GOOD',
+			'YOU MAY RELY ON IT',
+			'BETTER NOT TELL YOU NOW',
+			'SIGNS POINT TO YES'
 		]    
 		let randomAnswer = Math.floor(Math.random() * Math.floor(answers.length));
-		message.channel.send('I think `' + answers[randomAnswer] + '.`');
+		message.channel.send(answers[randomAnswer]);
 	},
 };
