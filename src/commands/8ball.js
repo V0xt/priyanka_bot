13c62d17aa74d8fda9b49ed250eddf744b197d7a
V0xt <alex.5363@ya.ru>
2019-10-11 18:35:32 +0300

    Modified 8ball command to get random answer from json

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -1,21 +1,12 @@
+const magicBall = require('../res/8ball.json');
+
 module.exports = {
 	name: '8ball',
 	usage: '!8ball',
 	aliases: '',
 	description: `8-Ball reaches into the future, to find the answers to your questions. It knows what will be, and is willing to share this with you.\nJust think of a question that can be answered "Yes" or "No", concentrate very, very hard, and type \'!8ball\'. Then let 8-Ball show you the way!`,
 	execute(message) {
-		let answers = [
-			'YES',
-			'NO',
-			'DEFINETLY NOT',
-			'MAYBE',
-			'WITHOUT A DOUBT',
-			'OUTLOOK NOT SO GOOD',
-			'YOU MAY RELY ON IT',
-			'BETTER NOT TELL YOU NOW',
-			'SIGNS POINT TO YES'
-		]    
-		let randomAnswer = Math.floor(Math.random() * Math.floor(answers.length));
-		message.channel.send(answers[randomAnswer]);
+		let item = magicBall.answers[Math.floor(Math.random() * magicBall.answers.length)];
+		message.channel.send(item);
 	},
 };
