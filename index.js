88ed3bac94854f633a763e317965d0e745b92049
V0xt <alex.5363@ya.ru>
2019-10-17 18:43:16 +0300

    New command

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ client.once('disconnect', () => {
 client.on('message', async (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	
-	const args = message.content.slice(prefix.length).split(' ');
+	const args = message.content.slice(prefix.length).split(/ +/);
 	const commandName = args.shift().toLowerCase();
 	const command = client.commands.get(commandName)
 		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
---
+++ src/commands/avatar.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: 'avatar',
+	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
+	guildOnly: false,
+	aliases: ['icon', 'pfp'],
+	usage: '!avatar',
+	cooldown: 5,
+
+	execute(message) {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}>`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+		});
+
+		message.channel.send(avatarList);
+	},
+};
