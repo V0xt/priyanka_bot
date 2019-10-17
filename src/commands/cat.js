77bd78b04c6418278bfde7ddbf7bb9e3346b9a9b
V0xt <alex.5363@ya.ru>
2019-10-17 17:47:32 +0300

    New command

---
+++ src/commands/cat.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+	name: 'cat',
+	description: 'Returns random cat picture from the aws.random.cat',
+	guildOnly: false,
+	//aliases: [],
+	usage: '!cat',
+	cooldown: 6,	
+
+	async execute(message) {
+		const { file }  = await fetch('https://aws.random.cat/meow').then(response => response.json());
+		message.channel.send(file);		
+	},
+};
+
