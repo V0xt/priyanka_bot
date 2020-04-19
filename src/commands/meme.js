d3eba9b4fb8167f123599e1f9e59bbd0ad426e40
Mona Lisa <alex.5363@ya.ru>
2020-04-19 20:34:27 +0300

    Returns random meme from https://imgflip.com/ By default, api request returns 100 memes, so we just random from 1 to 100 and reply with random meme

---
+++ src/commands/meme.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+	name: 'meme',
+	description: 'Returns random popular meme from imgflip.com',
+	guildOnly: false,
+	// aliases: [ ],
+	usage: '!meme',
+	cooldown: 10,
+
+	async execute(message) {
+		const result = await fetch('https://api.imgflip.com/get_memes').then(response => response.json());
+
+		const rnd = Math.floor(Math.random() * Math.floor(100));
+		message.channel.send(`${result.data.memes[rnd].url}`);
+	},
+};
