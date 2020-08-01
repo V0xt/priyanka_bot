f653e923226e540d64263b4954783bef98041d50
Aleksandr <alex.5363@ya.ru>
2020-08-01 17:03:15 +0300

    Add welcome message with Canvas

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const { CommandoClient } = require('discord.js-commando');
-const { Structures } = require('discord.js');
+const Discord = require('discord.js');
+const Canvas = require('canvas');
 const path = require('path');
 const fs = require('fs');
 
-Structures.extend('Guild', Guild => {
+Discord.Structures.extend('Guild', Guild => {
   class MusicGuild extends Guild {
     constructor(client, data) {
       super(client, data);
@@ -47,6 +48,51 @@ fs.readdir('./src/events/', (err, files) => {
     const eventName = file.split('.')[0];
     client.on(eventName, event.bind(null, client));
   });
+});
+
+const applyText = (canvas, text) => {
+  const ctx = canvas.getContext('2d');
+  let fontSize = 70;
+
+  do {
+    ctx.font = `${fontSize -= 10}px sans-serif`;
+  } while (ctx.measureText(text).width > canvas.width - 300);
+
+  return ctx.font;
+};
+
+client.on('guildMemberAdd', async (member) => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  if (!channel) return;
+
+  const canvas = Canvas.createCanvas(700, 250);
+  const ctx = canvas.getContext('2d');
+
+  const background = await Canvas.loadImage('./src/img/wallpaper.jpg');
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = '#74037b';
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+  ctx.font = '28px sans-serif';
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+  ctx.font = applyText(canvas, `${member.displayName}!`);
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+  ctx.beginPath();
+  ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
+
+  const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+  ctx.drawImage(avatar, 25, 25, 200, 200);
+
+  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+  channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
 client.login(process.env.BOT_TOKEN);
